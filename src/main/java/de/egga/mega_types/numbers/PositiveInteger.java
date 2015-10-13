cb454ca3e4484181e39ed1631e3b18f3a2ccf9a6
Egga (Jan Hartung) <github@egga.de>
2015-10-13 23:40:36 +0200

    expose validator

--- src/main/java/de/egga/mega_types/numbers/PositiveInteger.java
+++ src/main/java/de/egga/mega_types/numbers/PositiveInteger.java
@@ -2,17 +2,17 @@ package de.egga.mega_types.numbers;
 
 public class PositiveInteger {
 
+    public static void validate(int value) {
+        if (value < 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     private final int value;
 
     public PositiveInteger(int value) {
         validate(value);
         this.value = value;
-    }
-
-    private void validate(int value) {
-        if (value < 1) {
-            throw new IllegalArgumentException();
-        }
     }
 
     public int getValue() {
