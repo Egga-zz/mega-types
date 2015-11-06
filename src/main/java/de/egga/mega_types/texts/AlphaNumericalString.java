4061afee09a36a4fac0461037cc8559d9ae94f50
Egga (Jan Hartung) <github@egga.de>
2015-11-06 10:49:52 +0100

    add AlphaNumericalString

---
+++ src/main/java/de/egga/mega_types/texts/AlphaNumericalString.java
@@ -0,0 +1,24 @@
+package de.egga.mega_types.texts;
+
+import de.egga.mega_types.Value;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class AlphaNumericalString extends Value<String> {
+
+    public static final Pattern VALIDATION_PATTERN = Pattern.compile("^[a-zA-Z0-9]$");
+
+    public AlphaNumericalString(String value) {
+        super(value);
+    }
+
+    @Override
+    public void validate() {
+        Matcher matcher = VALIDATION_PATTERN.matcher(getValue());
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/de/egga/mega_types/texts/AlphaNumericalStringTest.java
@@ -0,0 +1,40 @@
+package de.egga.mega_types.texts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+@RunWith(Parameterized.class)
+public class AlphaNumericalStringTest {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"-"}, {" "}, {"_"}, {"*"}, {"."}, {"$"}, {"+"}
+        });
+    }
+
+    @Parameterized.Parameter
+    public String character;
+
+    @Test
+    public void it_should_return_value() {
+        AlphaNumericalString alphaNumericalString = new AlphaNumericalString("Marteria1337");
+        assertThat(alphaNumericalString.getValue()).isEqualTo("Marteria1337");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void it_should_fail_on_special_chars() {
+        try {
+            new AlphaNumericalString(character);
+            fail("didn't throw an exception!");
+        } catch (IllegalArgumentException e) {
+            // Test succeded!
+        }
+    }
+}
